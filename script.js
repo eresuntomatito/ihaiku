247e8403247607eb369772bf88ff403ff5e6f815
eresuntomatito <ldi.aaron@gmail.com>
2015-06-03 19:38:20 -0500

    Added CSS

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+console.log("hola");
+
+document.getElementById("boton1") = whenClick
+
+/*var item = document.getElementById("boton1");
+
+$(document).on('click','boton1',function(){
+
+});
+
+console.log(item);
+console.log(item.onclick);
+*/
+var whenClick = function () {
+	console.log("I'm alive!")
+};
+
+/*
+document.getElementById("color").onclick = changeColor;
+
+	var currentColor = "red";
+
+	function changeColor() {
+		console.log(currentColor)
+		
+		if(currentColor === "red"){
+			document.body.style.color = "green";
+			currentColor = "green"
+		} else {
+			document.body.style.color = "red";
+			currentColor = "red";
+		}
+		return currentColor;
+	}*/
